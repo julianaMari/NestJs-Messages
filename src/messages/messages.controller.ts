18284ea5ddcb14f7d99e4a08db4605f1936e314f
Juliana Cruz <julianamariacruz.jm@gmail.com>
2023-03-23 15:01:38 -0300

    alter docs

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,8 +1,16 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.services';
 
 @Controller('messages')
 export class MessagesController {
+    messagesService: MessagesService;
+
+    constructor() {
+        // DONT DO THIS ON REAL APP
+        // USE DEPENDENCY INJECTION
+        this.messagesService = new MessagesService();
+    }
 
     @Get()
     listMessages() {
